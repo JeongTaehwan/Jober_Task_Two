f2664ea75bfe000d0a77d03216cd02656b0fddb0
JeongTaehwan <wjdxoghks11222@naver.com>
2022-07-22 00:03:41 +0900

    Update : bookmark 컬럼 제거

--- src/components/leftNavigationBar/menus.js
+++ src/components/leftNavigationBar/menus.js
@@ -4,7 +4,6 @@ import {
   DesktopOutlined,
   ContainerOutlined,
   MailOutlined,
-  StarOutlined,
 } from "@ant-design/icons";
 
 export default [
@@ -12,30 +11,25 @@ export default [
     key: 1,
     label: "첫 번째 메뉴",
     icon: <AppstoreOutlined />,
-    bookmarkId: 1,
   },
   {
     key: 2,
     label: "두 번째 메뉴",
     icon: <PieChartOutlined />,
-    bookmarkId: 2,
   },
   {
     key: 3,
     label: "세 번째 메뉴",
     icon: <DesktopOutlined />,
-    bookmarkId: 3,
   },
   {
     key: 4,
     label: "네 번째 메뉴",
     icon: <ContainerOutlined />,
-    bookmarkId: 4,
   },
   {
     key: 5,
     label: "다섯 번째 메뉴",
     icon: <MailOutlined />,
-    bookmarkId: 5,
   },
 ];
